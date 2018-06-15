f80fd017a8b50bece8089e784c81b5a771dfbd99
Dan Lee <daniel.a.lee@capgemini.com>
2018-06-15 15:13:46 -0500

    added css stylings for all pages and javascript source file in src/main.js

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,6 +11,7 @@ import { Observable } from 'rxjs';
 
 export class HomeComponent implements OnInit {
   userForm: FormGroup;
+  loginForm: FormGroup;
   validMessage: String = '';
 
   constructor(private questionAppService: QuestionAppService) { }
@@ -22,13 +23,37 @@ export class HomeComponent implements OnInit {
       password: new FormControl('', Validators.required),
       firstName: new FormControl('', Validators.required),
       lastName: new FormControl('', Validators.required),
-      email: new FormControl('', Validators.email)
+      email: new FormControl('', [Validators.required, Validators.email])
+    });
+
+    this.loginForm = new FormGroup({
+      email: new FormControl('', Validators.email),
+      password: new FormControl('', [Validators.required, Validators.email])
     });
   }
 
   submitNewUser() {
     if (this.userForm.valid) {
       this.validMessage = 'You have successfully created a new user account!';
+      // pass in value of form (JSON user object) to the createUser(user: User) method in questionAppService.service
+      // angular will return an Observable that we can subscribe to even if Controller method returns void
+      this.questionAppService.createUser(this.userForm.value).subscribe(
+        data => {
+          this.userForm.reset();
+          return true;
+        },
+        error => {
+          return Observable.throw(error);
+        }
+      );
+    } else {
+      this.validMessage = 'Please complete the form before submitting!';
+    }
+  }
+
+  loginUser() {
+    if (this.userForm.valid) {
+      this.validMessage = 'You have successfully logged in!';
       // pass in value of form (JSON user object) to the createUser(user: User) method in questionAppService.service
       // angular will return an Observable that we can subscribe to even if Controller method returns void
       this.questionAppService.createUser(this.userForm.value).subscribe(
